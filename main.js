bddc3b2813fd3ab77091eeb9e3a8145c859a8c4a
mxt08-real <59659815+mxt08-real@users.noreply.github.com>
2020-09-02 16:23:04 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+navigator.geolocation.getCurrentPosition(a);
+function a(c) {
+    var d = "Latitude: ";
+    var e = "; Longitude: ";
+    var f = ";";
+    alert(d + c.coords.latitude + e + c.coords.longitude + f); }
